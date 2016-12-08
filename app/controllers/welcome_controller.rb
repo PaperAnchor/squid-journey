778c9345a99ea154de25c7ecb15219d24e332ef3
PaperAnchor <zach@paperanchor.ca>
2016-12-07 22:24:42 -0700

    revert contact changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,16 +3,4 @@ class WelcomeController < ApplicationController
             @posts = Post.all.limit(4).order("created_at desc")
             @contact = Contact.new
         end
-
-        def create
-            @contact = Contact.new(params[:contact])
-            @contact.request = request
-            if @contact.deliver
-                flash.now[:error] = nil
-            else
-                flash.now[:error] = "Cannot send message."
-                render :new
-            end
-        end
-
 end
