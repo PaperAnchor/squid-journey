ba1e3555582a582834447893f3fbee9469a09bdc
PaperAnchor <zach@paperanchor.ca>
2016-12-27 23:29:43 -0700

    Properly use S3 credential variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('zachmoodieblog'),
-    access_key_id: ENV.fetch('AKIAI4YWL4B7NYTEM4SQ'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('Oregon'),
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
   }
-}
 
 end
