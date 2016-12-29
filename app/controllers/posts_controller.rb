f46b38419cb028cab150465d32fe84ee15ee8fbc
PaperAnchor <zach@paperanchor.ca>
2016-12-28 23:18:09 -0700

    A ton of styling completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
-        @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
+        @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 5)
     end
 
     def new
@@ -18,7 +18,6 @@ class PostsController < ApplicationController
         else
             render 'new', notice: "Well, something went wrong. Try again?"
         end
-        @user = User.create( user_params )
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users
+
     resources :posts
     resources :contacts, only: [:new, :create]
     get 'welcome/index' #Defines the routes
