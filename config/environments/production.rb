59cce6f9b56e751b1c38273337f9888cad673222
PaperAnchor <zach@paperanchor.ca>
2016-12-29 09:26:28 -0700

    Fixed up SendGrid Configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  #Sets Sendgrid setup
+  config.action_mailer.default_url_options = { host: 'https://pure-lowlands-44628.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+    ActionMailer::Base.smtp_settings = {
+        :user_name => ENV ['SENDGRID_USERNAME'],
+        :password => ENV ['SENDGRID_PASSWORD'],
+        :domain => 'heroku.com',
+        :address => 'smtp.sendgrid.net',
+        :port => 587,
+        :authentication => :plain,
+        :enable_starttls_auto => true
+    }
+
   #Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
     storage: :s3,
