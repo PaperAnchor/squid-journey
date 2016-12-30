d4bef829a69249f38922a3be6cdcd9cd39f63892
PaperAnchor <zach@paperanchor.ca>
2016-12-29 23:32:14 -0700

    Styled the blog listing page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ActiveRecord::Base
     extend FriendlyId
     friendly_id :title, use: :slugged
-    #has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
-    has_attached_file :image
+    has_attached_file :image, styles: { small: "64x64", med: "500x321", large: "1200x770" }
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
